8bcc8e1677e073d6c41d25b8b9f4a1b00bb36359
Xander_ <alessandro.cevoli2@studio.unibo.it>
2018-06-06 12:25:50 +0200

    SVM refactor

--- SCTM_VR_Potholes_Detection/SVM.cpp
+++ SCTM_VR_Potholes_Detection/SVM.cpp
@@ -4,19 +4,25 @@
 
 #include "SVM.h"
 
-const double epsilon = exp(-6);
-
-Mat Classifier(vector<Features> &features, int max_iter, String model_path){
-
-    Mat labels_mat((int) features.size(), 1, CV_32SC1);
+Ptr<SVM> initSVM(String model_path, int max_iter, bool isTraining = false) {
+    const double epsilon = exp(-6);
 
     Ptr<SVM> svm = SVM::create();
 
-    svm->load(model_path);
+    if (!isTraining) {
+        try {
+            svm = svm->load(model_path);
+        } catch (Exception ex) {
+            printf(ex.msg.c_str());
+        }
+    }
 
     svm->setType(SVM::C_SVC);
     svm->setKernel(SVM::RBF);
     svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, max_iter, epsilon));
+}
+
+Mat ConvertFeatures(vector<Features> &features) {
 
     double data[features.size()][5];
 
@@ -30,37 +36,29 @@ Mat Classifier(vector<Features> &features, int max_iter, String model_path){
         data[i][4] = f.skewness;
     }
 
-    Mat data_mat((int) features.size(), 5, CV_32FC1 , data);
+    return Mat((int) features.size(), 5, CV_32FC1 , data);
+}
 
-    svm->predict(data_mat, labels_mat);
+Mat Classifier(vector<Features> &features, int max_iter, String model_path){
+
+    Mat labels_mat((int) features.size(), 1, CV_32SC1);
+    Mat data_mat = ConvertFeatures(features);
+
+    Ptr<SVM> svm = initSVM(model_path, max_iter);
+
+    auto flag = svm->predict(data_mat, labels_mat);
+
+    printf("FLAG: %2f.5", flag);
 
     return labels_mat;
 }
 
 void Training(vector<Features> &features, vector<int> &labels, int max_iter, String model_path) {
 
-    Ptr<SVM> svm = SVM::create();
+    Mat labels_mat((int) features.size(), 1, CV_32SC1, labels.data());
+    Mat data_mat = ConvertFeatures(features);
 
-    svm->load(model_path);
-
-    svm->setType(SVM::C_SVC);
-    svm->setKernel(SVM::RBF);
-    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, max_iter, epsilon));
-
-    double data[features.size()][5];
-
-    for (int i = 0; i < features.size(); ++i) {
-
-        Features f = features[i];
-        data[i][0] = f.averageGrayValue.val[0];
-        data[i][1] = f.contrast;
-        data[i][2] = f.energy;
-        data[i][3] = f.entropy;
-        data[i][4] = f.skewness;
-    }
-
-    Mat labels_mat = Mat((int) features.size(), 1, CV_32SC1, labels.data());
-    Mat data_mat((int) features.size(), 5, CV_32FC1 , data);
+    Ptr<SVM> svm = initSVM(model_path, max_iter, true);
 
     Ptr<TrainData> td = TrainData::create(data_mat, ROW_SAMPLE, labels_mat);
 

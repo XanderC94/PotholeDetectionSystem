9e360bfe40f31317911b479ac6d962eabd35a2aa
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-06-08 11:22:28 +0200

    REORGANIZED the features extraction

--- SCTM_VR_Potholes_Detection/FeaturesExtraction.cpp
+++ SCTM_VR_Potholes_Detection/FeaturesExtraction.cpp
@@ -4,19 +4,105 @@
 
 #include "FeaturesExtraction.h"
 
+
+typedef struct FeaturesVectors {
+    vector<float> averageGreyLevels = vector<float>();
+    vector<Mat> histograms = vector<Mat>();
+    vector<float> contrasts = vector<float>();
+    vector<float> entropies = vector<float>();
+    vector<float> skewnesses = vector<float>();
+    vector<float> energies = vector<float>();
+} FeaturesVectors;
+
+
+/*
+* Each centroid of superpixels extracted in the previous phase
+*  1. Candidate will be converted to greyscale
+*  2. The pothole region will be segmented another time with super pixeling
+*  3. The histogram will be calculated
+*  4. Calculate the average gray value
+*  5. Calculate the contrast
+*  6. Calculate 3-order moments (is Skewness according to http://aishack.in/tutorials/image-moments/)
+*  7. Calculate Consistency (After only searches I think that the consistency is what the professor calls energy)
+*  8. Calculate Entropy
+*  9. Put the calculated features in a eigenvector
+*
+* */
+Features candidateFeatureExtraction(Point centroid, Mat sourceImage, Size candidate_size) {
+    //tlc (top left corner) brc(bottom right corner)
+    auto tlc_x = centroid.x - candidate_size.width * 0.5;
+    auto tlc_y = centroid.y - candidate_size.height * 0.5;
+    auto brc_x = centroid.x + candidate_size.width * 0.5;
+    auto brc_y = centroid.y + candidate_size.height * 0.5;
+
+    auto tlc = Point2d(tlc_x < 0 ? 0 : tlc_x, tlc_y < 0 ? 0 : tlc_y);
+    auto brc = Point2d(brc_x > sourceImage.cols - 1 ? sourceImage.cols : brc_x,
+                       brc_y > sourceImage.rows - 1 ? sourceImage.rows : brc_y);
+
+    auto candidate = sourceImage(Rect(tlc, brc));
+
+    //candidates.push_back(candidate);
+    Mat candidateGrayScale;
+    // Switch color-space from RGB to GreyScale
+    cvtColor(candidate, candidateGrayScale, CV_BGR2GRAY);
+
+    // In order to reduce computation complexity
+    // calculate the contrast, entropy and energy with in the same loops
+
+    float contrast = 0.0;
+    float entropy = 0.0;
+    float energy = 0.0;
+
+    for (int i = 0; i < candidateGrayScale.rows; i++) {
+        for (int j = 0; j < candidateGrayScale.cols; j++) {
+            contrast = contrast + powf((i - j), 2) * candidateGrayScale.at<uchar>(i, j);
+            entropy = entropy + (candidateGrayScale.at<uchar>(i, j) * log10f(candidateGrayScale.at<uchar>(i, j)));
+            energy = energy + powf(candidateGrayScale.at<uchar>(i, j), 2);
+        }
+    }
+
+    energy = sqrtf(energy);
+    entropy = 0 - entropy;
+
+    float averageGreyValue = (float) mean(candidateGrayScale)[0];
+    float skewness = calculateSkewnessGrayImage(candidate, averageGreyValue);
+
+    auto c_name = "Candidate @ (" + to_string(centroid.x) + ", " + to_string(centroid.y) + ")";
+    Mat histogram = ExtractHistograms(candidateGrayScale, c_name);
+
+    return Features{candidate, histogram, averageGreyValue, contrast, entropy, skewness, energy};
+}
+
+FeaturesVectors normalizeFeatures(double minValue, double maxValue, FeaturesVectors notNormalizedFeatures) {
+    FeaturesVectors normalizedFeatures;
+
+    for (auto notNormHistogram : notNormalizedFeatures.histograms) {
+        Mat normHistogram;
+        normalize(notNormHistogram, normHistogram, minValue, maxValue, NORM_MINMAX, -1, Mat());
+        normalizedFeatures.histograms.push_back(normHistogram);
+    }
+
+    normalize(notNormalizedFeatures.averageGreyLevels, normalizedFeatures.averageGreyLevels, minValue, maxValue,
+              NORM_MINMAX, -1, Mat());
+    normalize(notNormalizedFeatures.contrasts, normalizedFeatures.contrasts, minValue, maxValue, NORM_MINMAX, -1,
+              Mat());
+    normalize(notNormalizedFeatures.entropies, normalizedFeatures.entropies, minValue, maxValue, NORM_MINMAX, -1,
+              Mat());
+    normalize(notNormalizedFeatures.skewnesses, normalizedFeatures.skewnesses, minValue, maxValue, NORM_MINMAX, -1,
+              Mat());
+    normalize(notNormalizedFeatures.energies, normalizedFeatures.energies, minValue, maxValue, NORM_MINMAX, -1, Mat());
+
+    return normalizedFeatures;
+}
+
+
 vector<Features> extractFeatures(Mat sourceImage, vector<Point> centroids, Size candidate_size) {
 
 //    auto candidates = vector<Mat>();
     auto notNormalizedfeatures = vector<Features>();
     auto normalizedFeatures = vector<Features>();
 
-    auto candidatesAverageGreyLevel = vector<float>();
-    auto candidatesContrast = vector<float>();
-    auto candidatesEntropy = vector<float>();
-    auto candidatesSkewness = vector<float>();
-    auto candidatesEnergy = vector<float>();
-
-    Mat candidateGrayScale;
+    FeaturesVectors candidatesFeaturesVectors;
 
     /*
         * Each centroid of superpixels extracted in the previous phase
@@ -32,93 +118,38 @@ vector<Features> extractFeatures(Mat sourceImage, vector<Point> centroids, Size 
         * */
     /*------------------------Candidate Extraction---------------------------*/
     for (auto c : centroids) {
-        //tlc (top left corner) brc(bottom right corner)
-        auto tlc_x = c.x - candidate_size.width * 0.5;
-        auto tlc_y = c.y - candidate_size.height * 0.5;
-        auto brc_x = c.x + candidate_size.width * 0.5;
-        auto brc_y = c.y + candidate_size.height * 0.5;
+        Features candidateFeatures = candidateFeatureExtraction(c, sourceImage, candidate_size);
+        notNormalizedfeatures.push_back(candidateFeatures);
 
-        auto tlc = Point2d(tlc_x < 0 ? 0 : tlc_x, tlc_y < 0 ? 0 : tlc_y);
-        auto brc = Point2d(brc_x > sourceImage.cols - 1 ? sourceImage.cols : brc_x,
-                           brc_y > sourceImage.rows - 1 ? sourceImage.rows : brc_y);
-
-        auto candidate = sourceImage(Rect(tlc, brc));
-
-//        candidates.push_back(candidate);
-
-        // Switch color-space from RGB to GreyScale
-        cvtColor(candidate, candidateGrayScale, CV_BGR2GRAY);
-
-        // In order to reduce computation complexity
-        // calculate the contrast, entropy and energy with in the same loops
-
-        float contrast = 0.0;
-        float entropy = 0.0;
-        float energy = 0.0;
-
-        for (int i = 0; i < candidateGrayScale.rows; i++) {
-            for (int j = 0; j < candidateGrayScale.cols; j++) {
-                contrast = contrast + powf((i - j), 2) * candidateGrayScale.at<uchar>(i, j);
-                entropy = entropy + (candidateGrayScale.at<uchar>(i, j) * log10f(candidateGrayScale.at<uchar>(i, j)));
-                energy = energy + powf(candidateGrayScale.at<uchar>(i, j), 2);
-            }
-        }
-
-        candidatesContrast.push_back(contrast);
-
-        energy = sqrtf(energy);
-        candidatesEnergy.push_back(energy);
-
-        entropy = 0 - entropy;
-        candidatesEntropy.push_back(entropy);
-
-        float averageGreyValue = (float) mean(candidateGrayScale)[0];
-        candidatesAverageGreyLevel.push_back(averageGreyValue);
-
-        float skewness = calculateSkewnessGrayImage(candidate, averageGreyValue);
-        candidatesSkewness.push_back(skewness);
-
-
-        auto c_name = "Candidate @ (" + to_string(c.x) + ", " + to_string(c.y) + ")";
-
-        Mat histogram = ExtractHistograms(candidateGrayScale, c_name);
-
-        normalize(histogram, histogram, 1, 10, NORM_MINMAX, -1, Mat());
-
-        notNormalizedfeatures.push_back(Features{candidate, histogram, averageGreyValue, contrast, entropy, skewness, energy});
+        candidatesFeaturesVectors.histograms.push_back(candidateFeatures.histogram);
+        candidatesFeaturesVectors.contrasts.push_back(candidateFeatures.contrast);
+        candidatesFeaturesVectors.energies.push_back(candidateFeatures.energy);
+        candidatesFeaturesVectors.entropies.push_back(candidateFeatures.entropy);
+        candidatesFeaturesVectors.averageGreyLevels.push_back(candidateFeatures.averageGreyValue);
+        candidatesFeaturesVectors.skewnesses.push_back(candidateFeatures.skewness);
     }
 
     /*------------- Normalization Phase -------------*/
 
-    auto normCandidatesAverageGreyLevel = vector<float>();
-    auto normCandidatesContrast = vector<float>();
-    auto normCandidatesEntropy = vector<float>();
-    auto normCandidatesSkewness = vector<float>();
-    auto normCandidatesEnergy = vector<float>();
-
     //Normalize feature values to [1,10]
-    normalize(candidatesAverageGreyLevel, normCandidatesAverageGreyLevel, 1, 10, NORM_MINMAX, -1, Mat());
-    normalize(candidatesContrast, normCandidatesContrast, 1, 10, NORM_MINMAX, -1, Mat());
-    normalize(candidatesEntropy, normCandidatesEntropy, 1, 10, NORM_MINMAX, -1, Mat());
-    normalize(candidatesSkewness, normCandidatesSkewness, 1, 10, NORM_MINMAX, -1, Mat());
-    normalize(candidatesEnergy, normCandidatesEnergy, 1, 10, NORM_MINMAX, -1, Mat());
+    FeaturesVectors normalizedFeaturesVectors = normalizeFeatures(1, 10, candidatesFeaturesVectors);
 
     for (int i = 0; i < centroids.size(); i++) {
-//        cout <<
-//             "Average Gray Value: " << normCandidatesAverageGreyLevel.at(i) <<
-//             " Contrast: " << normCandidatesContrast.at(i) <<
-//             " Skeweness: " << normCandidatesSkewness.at(i) <<
-//             " Energy: " << normCandidatesEnergy.at(i) <<
-//             " Entropy: " << normCandidatesEntropy.at(i) << endl;
+        cout <<
+             "Average Gray Value: " << normalizedFeaturesVectors.averageGreyLevels.at(i) <<
+             " Contrast: " << normalizedFeaturesVectors.contrasts.at(i) <<
+             " Skeweness: " << normalizedFeaturesVectors.skewnesses.at(i) <<
+             " Energy: " << normalizedFeaturesVectors.energies.at(i) <<
+             " Entropy: " << normalizedFeaturesVectors.entropies.at(i) << endl;
 
         normalizedFeatures.push_back(Features{
                 notNormalizedfeatures.at(i).candidate,
-                notNormalizedfeatures.at(i).histogram,
-                normCandidatesAverageGreyLevel.at(i),
-                normCandidatesContrast.at(i),
-                normCandidatesEntropy.at(i),
-                normCandidatesSkewness.at(i),
-                normCandidatesEnergy.at(i)
+                normalizedFeaturesVectors.histograms.at(i),
+                normalizedFeaturesVectors.averageGreyLevels.at(i),
+                normalizedFeaturesVectors.contrasts.at(i),
+                normalizedFeaturesVectors.entropies.at(i),
+                normalizedFeaturesVectors.skewnesses.at(i),
+                normalizedFeaturesVectors.energies.at(i)
         });
     }
 

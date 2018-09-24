46e8a5b7ece349a11d45e77b1b85adb8607b4e9f
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-09-24 18:30:19 +0200

    CLEANED comments in the CMakeLists file

--- src/io.cpp
+++ src/io.cpp
@@ -360,7 +360,7 @@ namespace phd::io {
 
         } else {
             cerr << "Program configuration is missing. Check it's existence or create a new config.json"
-                 << " under the ../res/config/ folder inside the program directory." << endl;
+                 << " under the ../res/config/ folder inside the program directory. " << endl;
 
             exit(-3);
         }
